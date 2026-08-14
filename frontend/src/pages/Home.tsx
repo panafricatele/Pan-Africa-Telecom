e023f16c9fc57a7fb82c96b27a2fbbf222df71b3
ntuthukosmithdev <ntuthukosmith10@gmail.com>
2026-08-14 10:48:45 +0200

    chore: comment out coverage checker on home page (using network status instead)

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import Header from '../components/Header';
 import HeroSection from '../components/HeroSection';
-import NetworkMap from '../components/NetworkMap';
+// import NetworkMap from '../components/NetworkMap';
 import ServiceExplorer from '../components/ServiceExplorer';
 import SpeedTestWidget from '../components/SpeedTestWidget';
 import ContactAndSupport from '../components/ContactAndSupport';
@@ -13,7 +13,8 @@ export default function Home() {
       <Header />
       <main className="flex-1">
         <HeroSection />
-        <NetworkMap />
+        {/* Coverage checker hidden while network status is in use */}
+        {/* <NetworkMap /> */}
         <section id="packages">
           <ServiceExplorer />
         </section>
